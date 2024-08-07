a791451374ef811529a9c3a377cc361a986d1e48
Natasza ASzatan <natiye.nb@gmai.com>
2024-08-07 10:37:16 +0100

    ran prettier

--- menu/menu.js
+++ menu/menu.js
@@ -4,4 +4,4 @@ import { RestaurantMenu } from "../RestaurantMenu.js";
 const productsServerAddress = "http://localhost:3000/products";
 const bodyContainer = document.getElementById("body-container");
 const restaurantMenu = new RestaurantMenu(bodyContainer, productsServerAddress);
-restaurantMenu.fetchProductsDataAndDisplayEditableMenu()
+restaurantMenu.fetchProductsDataAndDisplayEditableMenu();
