8433bc5f40ee016d6883032b3cf5c8989a3c3a29
Natasza ASzatan <natiye.nb@gmai.com>
2024-08-17 16:25:15 +0100

    developing Order Entry Class. constructor calls to create ordedr entry, first function to create insides of entry.

--- orders/OrderEntry.js
+++ orders/OrderEntry.js
@@ -1,3 +1,23 @@
 export class OrderEntry {
-  constructor() {}
+  constructor(ordersDataArray, listContainer, i) {
+    this.ordersDataAraay  = ordersDataArray;
+    this.listContainer = listContainer;
+    this.orderEntryNumber = i;
+    this.row = null;
+    this.createListEntry()
+  }
+  createListEntry() {
+    this.row = document.createElement("div");
+    this.row.classList.add("list-row");
+    this.createOrderIdDiv()
+    this.listContainer.append(this.row)
+  }
+  createOrderIdDiv(){
+    const title = document.createElement("div");
+    title.innerText = "Order ID:"
+    const orderID = document.createElement("div");
+    orderID.innerText = this.ordersDataAraay[this.orderEntryNumber].id;
+    this.row.append(title);
+    this.row.append(orderID)
+  }
 }
