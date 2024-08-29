dd3db0e7265e3c43b1390507a8dbbc6a0ae9c094
Natasza ASzatan <natiye.nb@gmai.com>
2024-08-29 14:29:35 +0200

    deleted next branch files

--- status/status.js
+++ status/status.js
@@ -1,13 +1,1 @@
-import "../style.css";
-import { UserStatusWindow } from "./UserStatusWindow.js";
-
-let url = new URL(window.location);
-let urlParams = new URLSearchParams(url.search);
-const paramId = urlParams.get("id");
-const serverAddress = "http://localhost:3000/";
-const bodyContainer = document.getElementById("body-container");
-const userStatusWindow = new UserStatusWindow(
-  paramId,
-  bodyContainer,
-  serverAddress,
-);
+console.log("script is working");
